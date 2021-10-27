c0c07141e4e4fb5d01b1a8debdab7963db648350
Juan-2 <negretenoguerajuanpablo@hotmail.com>
2021-10-27 14:25:01 -0500

    1.inicial

---
+++ app/src/main/java/com/minticgrupo08/grupo02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.minticgrupo08.grupo02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/minticgrupo08/grupo02/Mascotas.java
@@ -0,0 +1,14 @@
+package com.minticgrupo08.grupo02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascotas);
+    }
+}
