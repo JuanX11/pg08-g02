4f814890867021512398af29d5b5392347ce9943
Juan-2 <negretenoguerajuanpablo@hotmail.com>
2021-10-27 15:11:14 -0500

    2.Funcionalidad: Barra Navegación Inferior

---
+++ app/src/main/java/com/minticgrupo08/grupo02/Buscador.java
@@ -0,0 +1,49 @@
+package com.minticgrupo08.grupo02;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Buscador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscador);
+        //Barra de Navegación
+        //Iniciar y Crear Variables
+        BottomNavigationView bottomNavigationView = findViewById(R.id.barra_navegacion_inferior);
+        //Selección Principal
+        bottomNavigationView.setSelectedItemId(R.id.Buscador);
+
+        //Item Select Listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.Buscador:
+                        return true;
+
+                    case R.id.Inicio:
+                        startActivity(new Intent(getApplicationContext()
+                            ,Inicio.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                    case R.id.Mascotas:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Mascotas.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
--- app/src/main/java/com/minticgrupo08/grupo02/Mascotas.java
+++ app/src/main/java/com/minticgrupo08/grupo02/Mascotas.java
@@ -1,8 +1,13 @@
 package com.minticgrupo08.grupo02;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Mascotas extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class Mascotas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mascotas);
+        //Barra de Navegación
+         //Iniciar y Crear Variables
+            BottomNavigationView bottomNavigationView = findViewById(R.id.barra_navegacion_inferior);
+            //Selección Principal
+            bottomNavigationView.setSelectedItemId(R.id.Mascotas);
+
+            //Item Select Listener
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.Buscador:
+                            startActivity(new Intent(getApplicationContext()
+                                    ,Buscador.class));
+                            overridePendingTransition(0,0);
+                            return true;
+
+                        case R.id.Inicio:
+                            startActivity(new Intent(getApplicationContext()
+                                    ,Inicio.class));
+                            overridePendingTransition(0,0);
+                            return true;
+
+
+                        case R.id.Mascotas:
+                            return true;
+
+                    }
+                    return false;
+                }
+            });
     }
 }
