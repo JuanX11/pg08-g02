abe20c5361fda3439d7f72757a99232a2746846a
Dianalll78 <dilillamo78@gmail.com>
2021-11-17 21:04:57 -0500

    LoginActivity

--- app/src/main/java/com/minticgrupo08/grupo02/SplashActivity.java
+++ app/src/main/java/com/minticgrupo08/grupo02/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, Inicio.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             };
